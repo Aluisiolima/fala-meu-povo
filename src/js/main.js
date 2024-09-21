192897785f57afae670b0b4bf631d41d0dc8b6c5
aluisio <aaluisio309@gmail.com>
2024-09-21 12:08:59 -0300

    init de logica de como vai funcionar a requisicao dos componetes requer estudo sobre o async que nunca tinha usado ver mais tarde

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 function enviarProspota(){
     window.location.href = "./pages/tela2.html";
 }
+
+async function buscarComponentes(componente) {
+    try {
+        const response = await fetch(`./components/${componente}.html`);
+        if (!response.ok) {
+            console.error("NOT found componente");
+            return "NOT found componente";
+        }
+        const data = await response.text();
+        return data;
+    } catch (error) {
+        console.error("Error fetching componente:", error);
+        return "Error fetching componente";
+    }
+}
+
+const nav = document.getElementById('nav.componentes');
+buscarComponentes("nav").then(data => {
+    nav.innerHTML = data;
+});
